2fe2519d5c1582ff8b1b0e460fd9e496e283fdae
Umesh-Phendarkar <umesh.phendarkar@gmail.com>
2025-03-19 12:55:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,3 @@
-## Ci CD 
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = ">= 3.59.0"
-    } 
-  }
-  required_version = ">= 0.14.9"
-}
-
-provider "azurerm" {
-  features {}
-
-  skip_provider_registration = "true"
-}
-variable "prefix" {
-  default = "terraform"
-}
-
 terraform { 
   cloud { 
     
